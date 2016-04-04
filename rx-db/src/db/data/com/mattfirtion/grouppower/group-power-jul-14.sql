fbae13f948137081f80cbc8ddf8d6e4155ffe109
Matt Firtion <matt.firtion@coveros.com>
2016-04-03 21:13:11 -0400

    quote changes

--- rx-db/src/db/data/com/mattfirtion/grouppower/group-power-jul-14.sql
+++ rx-db/src/db/data/com/mattfirtion/grouppower/group-power-jul-14.sql
@@ -7,21 +7,5 @@ INSERT INTO track (track, artist, release_id, muscle_id) VALUES ('Boom Shack-A-L
 INSERT INTO track (track, artist, release_id, muscle_id) VALUES ('Epic', 'Faith No More', (select id from release where name = 'JUL 14' AND id = (select id from program where name = 'Group Power')), (select id from muscle where name = 'Biceps'));
 INSERT INTO track (track, artist, release_id, muscle_id) VALUES ('Wake Me Up!', 'Avicii', (select id from release where name = 'JUL 14' AND id = (select id from program where name = 'Group Power')), (select id from muscle where name = 'Legs 2'));
 INSERT INTO track (track, artist, release_id, muscle_id) VALUES ('Stomp!', 'DJ Aligator', (select id from release where name = 'JUL 14' AND id = (select id from program where name = 'Group Power')), (select id from muscle where name = 'Shoulders'));
-INSERT INTO track (track, artist, release_id, muscle_id) VALUES ("Chloe (You're The One I Want)", 'Emblem3', (select id from release where name = 'JUL 14' AND id = (select id from program where name = 'Group Power')), (select id from muscle where name = 'Core'));
+INSERT INTO track (track, artist, release_id, muscle_id) VALUES ('Chloe (You''re The One I Want)', 'Emblem3', (select id from release where name = 'JUL 14' AND id = (select id from program where name = 'Group Power')), (select id from muscle where name = 'Core'));
 INSERT INTO track (track, artist, release_id, muscle_id) VALUES ('Rebel Beat', 'Goo Goo Dolls', (select id from release where name = 'JUL 14' AND id = (select id from program where name = 'Group Power')), (select id from muscle where name = 'Stretch'));
-
-
-GroupPower – JUL 14
-
-01.[WARM-UP] Safe And Sound / Capital Cities
-02.[LEGS] Crazy / Seal
-03.[CHEST] Gonna Make You Sweat (Everybody Dance Now) / C+C Music Factory
-04.[BACK & LEGS] Applause / Lady Gaga
-05.[TRICEPS] Boom Shack-A-Lak / Apache Indian
-06.[BICEPS] Epic / Faith No More
-07.[MORE LEGS] Wake Me Up! / Avicii
-08.[SHOULDERS] Stomp! / DJ Aligator
-09.[CORE] Chloe (You’re The One I Want) / Emblem3
-10.[STRETCH] Rebel Beat / Goo Goo Dolls
-11.[BONUS LEGS(2)] You Give Love A Bad Name / Bon Jovi
-12.[BONUS BACK & LEGS(4)] Closer / Tegan and Sara
